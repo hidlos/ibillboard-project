0033af000cf27577912ef10b586eb2b29de4ece5
hidlos <adam.cink@email.cz>
2015-12-15 23:57:20 +0100

    Added condition, that checks that redisClient.incrby is function, otherwise throws error

--- source/redisUpdater.js
+++ source/redisUpdater.js
@@ -1,11 +1,13 @@
 
 function UpdateRedis(GETvariables,redisClient) {
   if(GETvariables.hasOwnProperty('count')) { 
-    redisClient.incrby('count', GETvariables.count, function(err, reply) {
-      redisClient.get("count", function(err,reply) {
-        console.log("nova hodnota parametru count je: "+reply);
+    if (typeof redisClient.incrby === 'function')
+      redisClient.incrby('count', GETvariables.count, function(err, reply) {
+        redisClient.get("count", function(err,reply) {
+          console.log("nova hodnota parametru count je: "+reply);
+        });
       });
-    });            
+    else throw new Error('redisClient is not defined');
   }; 
 }
 
